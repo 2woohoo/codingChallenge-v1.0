26640f74d8f8f5194d96d728806bb00972eeebbf
Ellie Wang <yuetingw@outlook.com>
2021-09-03 12:20:41 +0800

    add edit card function

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const inputTitle = document.querySelector('#titleInput');
 const inputDescr = document.querySelector('#descrInput');
 const addBtn = document.querySelector('.add-btn');
-const list = document.querySelector(".todoList");
+const todoList = document.querySelector(".todoList");
+const lists = document.querySelectorAll(".list");
+const cards = document.querySelectorAll('.card');
 
 inputTitle.onkeyup = () => {
   let userData = {
@@ -46,25 +48,25 @@ function showTasks(params) {
   else {
     listArray = JSON.parse(getLocalStorageData);
   }
-  console.log("lists", listArray);
-  let newLiTag = "";
+  // console.log("showtasks", listArray);
+  let newTag = "";
   listArray.forEach((e, index) => {
-    console.log("e",e.descr);
-    newLiTag += `<div class="card draggable" draggable="true">
+    // console.log("e",e.descr);
+    newTag += `<div class="card draggable" draggable="true">
             <div class="card-title">
               <h3>${e.title}</h3>
-              <input class="checkbox-effect checkbox-effect-1" id="get-up-1" type="checkbox" value="get-up-1" name="get-up-1" />
+              <input id="checkbox" type="checkbox" value="checkbox" name="checkbox" />
             </div>
             <p>${e.descr}</p>
             <div class="card-btns">
-              <i class="fas fa-pen"></i>
+              <i class="fas fa-pen" id="editBtn${index}" onclick="editTask(${index})"></i>
               <i class="fas fa-trash" onclick="deleteTask(${index})"></i>
             </div>
           </div>
         </div>`;
   });
 
-  list.innerHTML = newLiTag;
+  todoList.innerHTML = newTag;
   inputTitle.value = "";
   inputDescr.value = "";
 }
@@ -72,18 +74,74 @@ function showTasks(params) {
 function deleteTask(index) {
   let getLocalStorageData = localStorage.getItem("userTasks");
   listArray = JSON.parse(getLocalStorageData);
+  // console.log('delete',listArray);
   listArray.splice(index, 1);
   localStorage.setItem("userTasks", JSON.stringify(listArray));
   showTasks();
 }
 
-// drag and drop function
-const lists = document.querySelectorAll(".list");
-const cards = document.querySelectorAll('.card');
 
-cards.forEach((card) => {
+// edit task function
+cards.forEach((card,index) => {
+  editTask(index);
   registerEventsOnCard(card);
 });
+
+function editTask(index) {
+    const editBtn = document.querySelector(`#editBtn${ index }`);
+    let getLocalStorageData = localStorage.getItem("userTasks");
+    listArray = JSON.parse(getLocalStorageData);
+
+    editBtn.addEventListener('click',(e) => {
+      // console.log('test', e.currentTarget.parentNode.parentNode.childNodes[1].nextSibling.nextSibling,index);
+      if (editBtn.className === 'fas fa-pen') {
+        const cardTitle = e.currentTarget.parentNode.parentNode.childNodes[1];
+        const h3 = cardTitle.firstElementChild;
+        const p = cardTitle.nextSibling.nextSibling;
+        const titleInput = document.createElement('input');
+        const descrInput = document.createElement('input');
+        titleInput.type = 'text';
+        descrInput.type = 'text';
+        titleInput.value = h3.innerText;
+        descrInput.value = p.innerText;
+
+        cardTitle.insertBefore(titleInput,h3);
+        cardTitle.insertAdjacentElement("afterend", descrInput);
+
+        h3.remove();
+        p.remove();
+        editBtn.className = 'fas fa-save';
+
+      } else {
+        const cardTitle = e.currentTarget.parentNode.parentNode.childNodes[1];
+        const titleInput = cardTitle.firstElementChild;
+        const descrInput = e.currentTarget.parentNode.parentNode.childNodes[2];
+        const h3 = document.createElement('h3');
+        const p = document.createElement('p');
+        h3.textContent = titleInput.value;
+        p.textContent = descrInput.value;
+
+        cardTitle.insertBefore(h3, titleInput);
+        cardTitle.insertAdjacentElement("afterend", p);
+        console.log('127', p.textContent);
+        titleInput.remove();
+        descrInput.remove();
+
+        editBtn.className = 'fas fa-pen';
+        listArray[index].title = h3.textContent;
+        listArray[index].descr = p.textContent;
+
+        localStorage.setItem("userTasks", JSON.stringify(listArray));
+        showTasks();
+      }
+    });
+  }
+
+// drag and drop function
+
+// cards.forEach((card) => {
+//   registerEventsOnCard(card);
+// });
 
 lists.forEach((list) => {
   list.addEventListener('dragover', (e) => {
@@ -91,7 +149,6 @@ lists.forEach((list) => {
     let draggingCard = document.querySelector('.dragging');
     let cardAfterDraggingCard = getCardAfterDraggingCard(list, e.clientY);
     if (cardAfterDraggingCard) {
-
       cardAfterDraggingCard.parentNode.insertBefore(draggingCard, cardAfterDraggingCard);
     } else {
       list.appendChild(draggingCard);
